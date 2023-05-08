14d7b5cc413db883892552fa98970008ac82d5c9
Daniel2007ge <nxorbaladze@gmail.com>
2023-05-08 21:38:50 +0400

    new

---
+++ registration.js
@@ -0,0 +1,9 @@
+function func1() {
+    document.getElementsByTagName("div")[0].style.display = "none"
+    document.getElementsByTagName("form")[0].style.display = "block"
+}
+
+function func2() {
+    document.getElementsByTagName("div")[0].style.display = "none"
+    document.getElementsByTagName("form")[1].style.display = "block"
+}
