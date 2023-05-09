698b9edcead658ffa4156a850d7ff241913c2a32
Daniel2007ge <nxorbaladze@gmail.com>
2023-05-09 15:47:25 +0400

    new

--- registration.js
+++ registration.js
@@ -1,9 +1,35 @@
-function func1() {
-    document.getElementsByTagName("div")[0].style.display = "none"
-    document.getElementsByTagName("form")[0].style.display = "block"
+$(document).ready
+(
+    $("form").slideUp(0),
+
+    $("button:first").click(
+        function () {
+            $("div:first").slideUp(1000),
+            $("form:first").slideDown(1000)
+        }
+    ),
+
+    $($("button")[1]).click(
+        function () {
+            $("div:first").slideUp(1000),
+            $($("form")[1]).slideDown(1000)
+        }
+    )
+)
+
+function func1(input) {
+    input.nextElementSibling.style = "opacity: 1"
 }
 
-function func2() {
-    document.getElementsByTagName("div")[0].style.display = "none"
-    document.getElementsByTagName("form")[1].style.display = "block"
+let x = 0
+function func2(img) {
+    if(x == 0){
+        img.previousElementSibling.type = "text"
+        img.src = "images/eye1.png"
+        x = 1
+    }else{
+        img.previousElementSibling.type = "password"
+        img.src = "images/eye2.png"
+        x = 0
+    }
 }
