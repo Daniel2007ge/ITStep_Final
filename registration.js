e79eecdb7951717d074aaf27b1d9358829dfcbbd
Daniel2007ge <nxorbaladze@gmail.com>
2023-05-10 18:50:20 +0400

    new

--- registration.js
+++ registration.js
@@ -35,21 +35,41 @@ function func2(img) {
 }
 
 $("#login").click(function () {
-    $.getJSON("data.json",
-    function (data) {
-        let arr = data.Users
+    $.get("data.js",
+    function () {
+        let arr = Users
         let input1 = $("input")[0]
         let input2 = $("input")[1]
         for (let index = 0; index < arr.length; index++) {
             if((input1.value==arr[index].name) && (input2.value==arr[index].password)){
                 localStorage.setItem("user", JSON.stringify(arr[index]))
                 window.open("UserPage.html")
-                // if (condition) {
-                //     localStorage.setItem("user", JSON.stringify(arr[index]))
-                //     window.open("index2.html")
-                // }
             }
         }
         window.open("ErorPage.html")
     })
 })
+
+$("#signup").click(function () {
+    let inputName = $("input")[2].value
+    let inputEmail = $("input")[3].value
+    let inputPassword = $("input")[4].value
+    let Role = document.getElementsByTagName("select")[0].value
+    $.get("data.js",
+        function () {            
+            if ((inputName == "") && (inputEmail == "") && (inputPassword == "")) {
+                alert("fill the gaps")
+            }else {
+                Users.forEach(element => {
+                    if ((inputName == element.name) && (inputEmail == element.email)) {
+                        alert("Account with that kind of name or email is already existed")
+                    }else {
+                        Users.push({"name":inputName,"role":Role,"email":inputEmail,"password":inputPassword})
+                        localStorage.setItem("user" + (Users.length + 1), JSON.stringify({"name":inputName,"role":Role,"email":inputEmail,"password":inputPassword}))
+                        console.log(Users)
+                    } 
+                });
+            }
+        },
+    );
+});
