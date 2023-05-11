fff96bbc22b0dbcffda3492d37773265d720bc9f
Daniel2007ge <nxorbaladze@gmail.com>
2023-05-11 17:50:45 +0400

    new

--- data.js
+++
@@ -1,7 +0,0 @@
-let Users = [
-    {name:"Gabriel", role:"Business Client", email:"Gabriel@gmail.com", password:"password1"},
-    {name:"Emma", role:"User", email:"Emma@gmail.com", password:"password2"},
-    {name:"Rose", role:"Business Client", email:"Rose@gmail.com", password:"password3"},
-    {name:"Leo", role:"User", email:"Leo@gmail.com", password:"password4"},
-    {name:"Arthur", role:"User", email:"Arthur@gmail.com", password:"password5"},
-]
