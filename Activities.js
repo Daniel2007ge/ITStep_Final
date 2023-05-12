cdc038a3bcc8c4bae3a2d28ed84f2b7938f97be4
Daniel2007ge <nxorbaladze@gmail.com>
2023-05-12 20:24:43 +0400

    new

---
+++ Activities.js
@@ -0,0 +1,14 @@
+let card = globalVariable.Activities
+let div = document.getElementsByTagName("div")[0]
+console.log(card[0])
+for (let index = 0; index < card.length; index++) {
+    let figure = document.createElement("figure")
+    figure.setAttribute("class", "fig_4")
+    let img = document.createElement("img")
+    img.setAttribute("src", card[index].img)
+    let figcaption = document.createElement("figcaption")
+    figcaption.append(card[index].title)
+    figure.append(img)
+    figure.append(figcaption)
+    div.append(figure)
+}
