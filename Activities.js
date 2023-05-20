474494265153dd6e20bdb4cec0ba6d369192bf65
Daniel2007ge <nxorbaladze@gmail.com>
2023-05-20 15:32:09 +0400

    new

--- Activities.js
+++ Activities.js
@@ -7,7 +7,18 @@ for (let index = 0; index < Activities.length; index++) {
     img.setAttribute("src", Activities[index].img)
     let figcaption = document.createElement("figcaption")
     figcaption.append(Activities[index].title)
+    let button = document.createElement("button")
+    button.setAttribute("onclick", "func1("+index+")")
+    button.innerText = "find"
     figure.append(img)
     figure.append(figcaption)
+    figure.append(button)
     div.append(figure)
+}
+
+function func1(index) {
+    localStorage.setItem("type", "hotel")
+    localStorage.setItem("place", "")
+    localStorage.setItem("active", document.getElementsByTagName("figcaption")[index].innerHTML)
+    window.open("Hotels.html")
 }
