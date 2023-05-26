e53fba4e5b36c3137cc9fec51d5a1e66d1937248
Daniel2007ge <nxorbaladze@gmail.com>
2023-05-26 22:27:14 +0400

    new

--- Activities.js
+++ Activities.js
@@ -1,4 +1,4 @@
-let div = document.getElementsByTagName("div")[0]
+let div = document.getElementsByTagName("div")[1]
 
 for (let index = 0; index < Activities.length; index++) {
     let figure = document.createElement("figure")
@@ -16,9 +16,34 @@ for (let index = 0; index < Activities.length; index++) {
     div.append(figure)
 }
 
+if (localStorage.getItem("user") != null) {
+    document.getElementsByClassName("home_tag_a")[0].setAttribute("href", "UserPage.html")
+    document.getElementsByClassName("home_tag_a")[1].setAttribute("href", "UserPage.html")
+}
+
 function func1(index) {
     localStorage.setItem("type", "hotel")
     localStorage.setItem("place", "")
     localStorage.setItem("active", document.getElementsByTagName("figcaption")[index].innerHTML)
     window.open("Hotels.html")
+}
+
+function funcOpen(e){
+    if (e == 0) {
+        localStorage.setItem("type", "hotel")
+    } else {
+        localStorage.setItem("type", "restaurant")
+    }
+    localStorage.setItem("place", "")
+    localStorage.setItem("active", "")
+    window.open("Hotels.html")
+    window.close("home.html")
+    window.close("UserPage.html")
+}
+
+function ShowHide() {
+    for (let index = 0; index < 7; index++) {
+        document.getElementsByClassName("slide_a")[index].style.display = "block"
+    }
+    $("#slide").slideToggle(500)
 }
