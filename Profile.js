197016ad693571d8a73879db268c09859c172d83
Daniel2007ge <nxorbaladze@gmail.com>
2023-05-29 16:24:15 +0400

    new

--- Profile.js
+++ Profile.js
@@ -1,18 +1,25 @@
 let obj1 = JSON.parse(localStorage.getItem("user"))
 
-let obj2 = JSON.parse(localStorage.getItem("served"))
-document.getElementsByTagName("img")[0].setAttribute("src", obj2.Img)
-document.getElementsByTagName("figcaption")[0].innerText = obj2.Name
-document.getElementsByClassName("star")[0].setAttribute("src", obj2.Stars)
-document.getElementsByTagName("p")[1].innerHTML = "<span>location: </span>" + obj2.Location
-document.getElementsByTagName("p")[2].innerHTML = "<span>guests count: </span>" + obj2.Guests
-document.getElementsByTagName("p")[3].innerHTML = "<span>date of visit: </span>" + obj2.Visiting
-document.getElementsByTagName("p")[4].innerHTML = "<span>date of check out: </span>" + obj2.CheckingOut
+if(localStorage.getItem("served") == null){
+    document.getElementsByTagName("figure")[0].style.display = "none"
+    let h1 = document.createElement("h1")
+    h1.innerText = "You have not served any hotel or restaurant"
+    document.getElementsByTagName("section")[1].append(h1)
+}else {
+    let obj2 = JSON.parse(localStorage.getItem("served"))
+    document.getElementsByTagName("img")[0].setAttribute("src", obj2.Img)
+    document.getElementsByTagName("figcaption")[0].innerText = obj2.Name
+    document.getElementsByClassName("star")[0].setAttribute("src", obj2.Stars)
+    document.getElementsByTagName("p")[1].innerHTML = "<span>location: </span>" + obj2.Location
+    document.getElementsByTagName("p")[2].innerHTML = "<span>guests count: </span>" + obj2.Guests
+    document.getElementsByTagName("p")[3].innerHTML = "<span>date of visit: </span>" + obj2.Visiting
+    document.getElementsByTagName("p")[4].innerHTML = "<span>date of check out: </span>" + obj2.CheckingOut
 
-document.getElementsByTagName("input")[0].value = obj1.firstName
-document.getElementsByTagName("input")[1].value = obj1.lastName
-document.getElementsByTagName("input")[2].value = obj1.email
-document.getElementsByTagName("input")[3].value = obj1.password
+    document.getElementsByTagName("input")[0].value = obj1.firstName
+    document.getElementsByTagName("input")[1].value = obj1.lastName
+    document.getElementsByTagName("input")[2].value = obj1.email
+    document.getElementsByTagName("input")[3].value = obj1.password
+}
 
 if (localStorage.getItem("user") != null) {
     document.getElementsByClassName("home_tag_a")[0].setAttribute("href", "UserPage.html")
